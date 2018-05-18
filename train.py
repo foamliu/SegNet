a103fc297c7523c780457512542611927a270684
Yang Liu <foamliu@yeah.net>
2018-05-18 16:28:24 +0800

    monitor val_acc

--- train.py
+++ train.py
@@ -7,17 +7,17 @@ import migrate
 from config import patience, batch_size, epochs, num_train_samples, num_valid_samples
 from data_generator import train_gen, valid_gen
 from model import build_encoder_decoder
-from utils import get_available_gpus
+from utils import get_available_gpus, get_available_cpus
 
 if __name__ == '__main__':
     checkpoint_models_path = 'models/'
 
     # Callbacks
     tensor_board = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
-    model_names = checkpoint_models_path + 'model.{epoch:02d}-{val_loss:.4f}.hdf5'
-    model_checkpoint = ModelCheckpoint(model_names, monitor='val_loss', verbose=1, save_best_only=True)
-    early_stop = EarlyStopping('val_loss', patience=patience)
-    reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=int(patience / 4), verbose=1)
+    model_names = checkpoint_models_path + 'model.{epoch:02d}-{val_acc:.4f}.hdf5'
+    model_checkpoint = ModelCheckpoint(model_names, monitor='val_acc', verbose=1, save_best_only=True)
+    early_stop = EarlyStopping('val_acc', patience=patience)
+    reduce_lr = ReduceLROnPlateau('val_acc', factor=0.1, patience=int(patience / 4), verbose=1)
 
 
     class MyCbk(keras.callbacks.Callback):
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
         def on_epoch_end(self, epoch, logs=None):
             fmt = checkpoint_models_path + 'model.%02d-%.4f.hdf5'
-            self.model_to_save.save(fmt % (epoch, logs['val_loss']))
+            self.model_to_save.save(fmt % (epoch, logs['val_acc']))
 
 
     num_gpu = len(get_available_gpus())
@@ -43,7 +43,7 @@ if __name__ == '__main__':
         model = build_encoder_decoder()
         migrate.migrate_model(model)
 
-    model.compile(optimizer='nadam', loss='categorical_crossentropy')
+    model.compile(optimizer='nadam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     print(model.summary())
 
@@ -59,5 +59,5 @@ if __name__ == '__main__':
                         verbose=1,
                         callbacks=callbacks,
                         use_multiprocessing=True,
-                        workers=4
+                        workers=int(round(get_available_cpus() / 2))
                         )
