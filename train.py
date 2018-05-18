1243cc937263a6fea4c9a3a71307ab0401970b66
Yang Liu <foamliu@yeah.net>
2018-05-18 15:36:47 +0800

    to be greedy, larger batch size

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
         def on_epoch_end(self, epoch, logs=None):
             fmt = checkpoint_models_path + 'model.%02d-%.4f.hdf5'
             self.model_to_save.save(fmt % (epoch, logs['val_loss']))
-            
+
 
     num_gpu = len(get_available_gpus())
     if num_gpu >= 2:
