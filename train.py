7467acc4aa836b4644b4b299e5d0c2fb7591f5c0
Yang Liu <foamliu@yeah.net>
2018-05-18 15:31:18 +0800

    multi-gpus

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 import keras
+import tensorflow as tf
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from keras.utils import multi_gpu_model
 
 import migrate
 from config import patience, batch_size, epochs, num_train_samples, num_valid_samples
 from data_generator import train_gen, valid_gen
 from model import build_encoder_decoder
+from utils import get_available_gpus
 
 if __name__ == '__main__':
     checkpoint_models_path = 'models/'
@@ -16,8 +19,29 @@ if __name__ == '__main__':
     early_stop = EarlyStopping('val_loss', patience=patience)
     reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=int(patience / 4), verbose=1)
 
-    model = build_encoder_decoder()
-    migrate.migrate_model(model)
+
+    class MyCbk(keras.callbacks.Callback):
+        def __init__(self, model):
+            keras.callbacks.Callback.__init__(self)
+            self.model_to_save = model
+
+        def on_epoch_end(self, epoch, logs=None):
+            fmt = checkpoint_models_path + 'model.%02d-%.4f.hdf5'
+            self.model_to_save.save(fmt % (epoch, logs['val_loss']))
+            
+
+    num_gpu = len(get_available_gpus())
+    if num_gpu >= 2:
+        with tf.device("/cpu:0"):
+            model = build_encoder_decoder()
+            migrate.migrate_model(model)
+
+        new_model = multi_gpu_model(model, gpus=num_gpu)
+        # rewrite the callback: saving through the original model and not the multi-gpu model.
+        model_checkpoint = MyCbk(model)
+    else:
+        model = build_encoder_decoder()
+        migrate.migrate_model(model)
 
     model.compile(optimizer='nadam', loss='categorical_crossentropy')
 
---
+++ utils.py
@@ -0,0 +1,6 @@
+from tensorflow.python.client import device_lib
+
+# getting the number of GPUs
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
