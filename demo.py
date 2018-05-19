9566e8cbcc0ff27d5858650c4b75794f824e4db2
Yang Liu <foamliu@yeah.net>
2018-05-19 20:58:07 +0800

    __________________________

--- demo.py
+++ demo.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
         out = np.reshape(out, (img_rows, img_cols, num_labels))
         out = np.argmax(out, axis=2)
         print("out.shape: " + str(out.shape))
-        ret = np.array((img_rows, img_cols, 3), np.float32)
+        ret = np.zeros((img_rows, img_cols, 3), np.float32)
         for r in range(320):
             for c in range(320):
                 color_id = out[r, c]
